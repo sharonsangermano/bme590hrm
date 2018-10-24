b9d8da69d695424e1644cf725d6bb85708e0fa2e
Sharon Sangermano <sharon.sangermano@duke.edu>
2018-10-23 22:54:15 -0400

    First take of test files.
    Adds test files containing test functions for all current functions.  In
    the future test functions would be created simulatneously with function
    definitions.  This version of the test functions does not use the proper
    format for testing functions within a class.

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from readdata import ReadData
+import readdata
 from processdata import ProcessData
 
 
 def main():
-    current_data = ReadData('test_data/test_data1.csv')
-    time, voltage = current_data.get_data()
+    num = input('Enter test data file number')
+    time, voltage = readdata.get_data('test_data/test_data' + num + '.csv')
+    #time, voltage = readdata.get_data('test_data/test_data1.csv')
     ecg_series = ProcessData(time, voltage)
     print(time)
     print(voltage)
@@ -12,11 +13,11 @@ def main():
     print(ecg_series.get_voltage())
     print('The minimum voltage is', ecg_series.get_min(), 'V')
     print('The maximum voltage is', ecg_series.get_max(), 'V')
-    print('The duration is', ecg_series.get_duration(), 's')
     print(ecg_series.get_peaks())
     print(ecg_series.get_num_beats())
     print(ecg_series.get_beats_time())
     print(ecg_series.get_peak_voltage())
+    print('The duration is %.3f s' % ecg_series.get_duration())
     print('The mean heart rate is %.3f bpm' % ecg_series.get_mean_hr())
 
 
